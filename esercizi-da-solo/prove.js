6faf450f90b9014a74a46be80c757df81c9b11a8
Francesco Antoni <f.antoni@consorziometis.it>
2025-03-10 12:49:45 +0100

    aggiunti selectionSort e insertion lasciato a metà

--- esercizi-da-solo/prove.js
+++ esercizi-da-solo/prove.js
@@ -33,15 +33,44 @@
 // console.log(v.__proto__)
 // console.log(Object.getPrototypeOf(v))
 
-function Persona(n, e) {
-    this.nome = n
-    this.età = e
+// function Persona(n, e) {
+//     this.nome = n
+//     this.età = e
+// }
+//
+// Persona.prototype.compleanno = function () {
+//     this.età++
+// }
+//
+// let pippo = new Persona("Pippo", 35)
+// pippo.compleanno()
+// console.log(pippo)
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let lowest = i
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowest]) {
+                lowest = j
+            }
+        }
+        if (lowest !== i) {
+            // Swap
+            ;[arr[i], arr[lowest]] = [arr[lowest], arr[i]]
+        }
+    }
+    return arr
 }
 
-Persona.prototype.compleanno = function () {
-    this.età++
+function insertionSort(arr) {
+    for (let j = 0; j < arr.length; j++) {
+        let key = arr[j]
+        let i = j - 1
+        while (i > -1 && arr[i] > key){
+            arr[]
+        }
+    }
 }
 
-let pippo = new Persona("Pippo", 35)
-pippo.compleanno()
-console.log(pippo)
+let v = [10, 9, 7, 22, 15, 1]
+console.log(insertionSort(v))
